ec8de8a5325eeb20e83e0d244b9c16e9f3cf0088
Helana <helanan@gmail.com>
2017-04-16 19:06:58 -0500

    added new docs

---
+++ margaret.py
@@ -0,0 +1,18 @@
+# import sys
+# from mary import Mary
+
+# Create a mary instance
+
+# margaret = Mary()
+# margaret.message = user_input
+#
+# serialize
+# deserialize
+#
+# add_to_dict
+
+# sets
+message_one = print(set("Hi, my name is Mary".split()))
+message_two = print(set("Im good, how are you?".split()))
+message_three = print(set("What would you like to do today?".split()))
+message_four = print(set("That sounds great!".split()))
